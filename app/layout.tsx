d09f20b21214a455fadca8b12c9013d0451def21
Stephanie Lin <stephanie_lin@brown.edu>
2026-02-26 18:57:15 -0500

    tools-resources lofi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ResourcesAndTools from "./tools-resources";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ResourcesAndTools></ResourcesAndTools>
       </body>
     </html>
   );
