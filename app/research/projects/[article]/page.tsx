e8f5b0fba9a3d820fd4ed71d980003cd7e0903d6
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2026-03-06 14:32:02 -0500

    basic css slug page

--- app/research/projects/[article]/page.tsx
+++ app/research/projects/[article]/page.tsx
@@ -1,6 +1,40 @@
 // This is a component to render each page of research 
 import mockData from '../mockData';
 
+function getContent(project: any) {
+    /** @type {string[]} */
+    const content = project.content;
+    const margin = "my-2";
+
+    return (
+        <div>
+            {content.map((item: any, i: number) => {
+                switch (item.type) {
+
+                case "paragraph":
+                    return <p key={i} className={margin}>{item.text}</p>;
+
+                case "header":
+                    return <div key={i} className={margin + " text-md font-bold"}>{item.text}</div>;
+
+                case "list":
+                    return (
+                    <ul key={i} className={margin + " list-disc pl-5"}>
+                        {item.items.map((subitem: string, j: number) => (
+                            <li key={j}
+                            className={margin}
+                            >{subitem}</li>
+                        ))}
+                    </ul>
+                    );
+
+                default:
+                    return null;
+            }})}
+        </div>
+    )
+}
+
 export default async function ProjectPage({params}: {params: Promise<{article: string}>}) {
     const { article } = await params;
 
@@ -12,8 +46,17 @@ export default async function ProjectPage({params}: {params: Promise<{article: s
 
     return (
         <div>
-            <img src={project.image}></img>
-            <h1>{project.Title}</h1>
+            <div className="block relative w-full h-full overflow-hidden max-h-65">
+                <img className="w-full h-64 object-cover" src={project.image}></img>
+                <h1 className="absolute bottom-15 left-55 text-white text-4xl font-bold">{project.Title}</h1>
+                <p className="absolute bottom-10 left-8 text-white text-xs">{project.description}</p>
+            </div>
+
+            <div className="grid grid-cols-4">
+                <div className="col-start-2 col-span-2">
+                    {getContent(project)}
+                </div>
+            </div>
         </div>
     );
 }
--- app/research/projects/mockData.ts
+++ app/research/projects/mockData.ts
@@ -6,13 +6,96 @@ const mockData = [
       image: '/mock-project-img.png',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.',
-    },
+    
+      content: [
+            {
+              type: "paragraph",
+              text: "We know from early Roman mosaics that physical rehabilitation and amplification technologies have been used during much of recorded history."
+            },
+            {
+              type: "paragraph",
+              text: "Although the goal of constructing such technologies is not new, great scientific and technological hurdles still remain."
+            },
+            {
+              type: "header",
+              level: 2,
+              text: "Equity and Justice in Biomechatronics Culture"
+            },
+            {
+              type: "paragraph",
+              text: "The Biomechatronics Group within the MIT Media Lab seeks to embody the values of open-mindedness, mutual respect, and collaboration through our community and research."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "The Biomechatronics Group is committed to creating an environment that reflects the diversity of society.",
+                "Talented and passionate scientists, engineers and designers are sought regardless of background.",
+                "Underrepresented minorities are encouraged to apply."
+              ]
+            },
+            {
+              type: "paragraph",
+              text: "Without diversity of skills, thought, and experiences, we lessen our ability to ask important questions and innovate."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "Improve dialogue and respect amongst distinct groups",
+                "Value varied and intersectional backgrounds",
+                "Deconstruct stereotypes"
+              ]
+            }
+          ]
+        },
     {
       slug: 'mock-2',
       Title: 'Mock 2',
       subtitle: 'Grant Opportunity',
       image: '/mock-project-img.png',
       description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
+      content: [
+          {
+            type: "paragraph",
+            text: "We know from early Roman mosaics that physical rehabilitation and amplification technologies have been used during much of recorded history."
+          },
+          {
+            type: "paragraph",
+            text: "Although the goal of constructing such technologies is not new, great scientific and technological hurdles still remain."
+          },
+          {
+            type: "header",
+            level: 2,
+            text: "Equity and Justice in Biomechatronics Culture"
+          },
+          {
+            type: "paragraph",
+            text: "The Biomechatronics Group within the MIT Media Lab seeks to embody the values of open-mindedness, mutual respect, and collaboration through our community and research."
+          },
+          {
+            type: "list",
+            style: "unordered",
+            items: [
+              "The Biomechatronics Group is committed to creating an environment that reflects the diversity of society.",
+              "Talented and passionate scientists, engineers and designers are sought regardless of background.",
+              "Underrepresented minorities are encouraged to apply."
+            ]
+          },
+          {
+            type: "paragraph",
+            text: "Without diversity of skills, thought, and experiences, we lessen our ability to ask important questions and innovate."
+          },
+          {
+            type: "list",
+            style: "unordered",
+            items: [
+              "Improve dialogue and respect amongst distinct groups",
+              "Value varied and intersectional backgrounds",
+              "Deconstruct stereotypes"
+            ]
+          }
+        ]
     },
     {
       slug: 'mock-3',
@@ -21,6 +104,47 @@ const mockData = [
       image: '/mock-project-img.png',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.',
+        content: [
+            {
+              type: "paragraph",
+              text: "We know from early Roman mosaics that physical rehabilitation and amplification technologies have been used during much of recorded history."
+            },
+            {
+              type: "paragraph",
+              text: "Although the goal of constructing such technologies is not new, great scientific and technological hurdles still remain."
+            },
+            {
+              type: "header",
+              level: 2,
+              text: "Equity and Justice in Biomechatronics Culture"
+            },
+            {
+              type: "paragraph",
+              text: "The Biomechatronics Group within the MIT Media Lab seeks to embody the values of open-mindedness, mutual respect, and collaboration through our community and research."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "The Biomechatronics Group is committed to creating an environment that reflects the diversity of society.",
+                "Talented and passionate scientists, engineers and designers are sought regardless of background.",
+                "Underrepresented minorities are encouraged to apply."
+              ]
+            },
+            {
+              type: "paragraph",
+              text: "Without diversity of skills, thought, and experiences, we lessen our ability to ask important questions and innovate."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "Improve dialogue and respect amongst distinct groups",
+                "Value varied and intersectional backgrounds",
+                "Deconstruct stereotypes"
+              ]
+            }
+          ]
     },
     {
       slug: 'mock-4',
@@ -29,6 +153,47 @@ const mockData = [
       image: '/mock-project-img.png',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.',
+        content: [
+            {
+              type: "paragraph",
+              text: "We know from early Roman mosaics that physical rehabilitation and amplification technologies have been used during much of recorded history."
+            },
+            {
+              type: "paragraph",
+              text: "Although the goal of constructing such technologies is not new, great scientific and technological hurdles still remain."
+            },
+            {
+              type: "header",
+              level: 2,
+              text: "Equity and Justice in Biomechatronics Culture"
+            },
+            {
+              type: "paragraph",
+              text: "The Biomechatronics Group within the MIT Media Lab seeks to embody the values of open-mindedness, mutual respect, and collaboration through our community and research."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "The Biomechatronics Group is committed to creating an environment that reflects the diversity of society.",
+                "Talented and passionate scientists, engineers and designers are sought regardless of background.",
+                "Underrepresented minorities are encouraged to apply."
+              ]
+            },
+            {
+              type: "paragraph",
+              text: "Without diversity of skills, thought, and experiences, we lessen our ability to ask important questions and innovate."
+            },
+            {
+              type: "list",
+              style: "unordered",
+              items: [
+                "Improve dialogue and respect amongst distinct groups",
+                "Value varied and intersectional backgrounds",
+                "Deconstruct stereotypes"
+              ]
+            }
+          ]
     },
   ];
 
