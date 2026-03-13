17fe528ae9ffe523cb84bf6f7026c0ae74b9d92c
honeywellfan <miriam_tesfaye@brown.edu>
2026-03-13 14:17:45 -0400

    basic sanity type for people made

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-import { blockContentType } from './blockContentType'
-import { exampleDatasetType } from './exampleDatasetType'
-import { examplePersonType } from './examplePersonType'
+import { blockContentType } from "./blockContentType";
+import { exampleDatasetType } from "./exampleDatasetType";
+import { examplePersonType } from "./examplePersonType";
+import { peopleType } from "./peopleType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, examplePersonType, exampleDatasetType],
-}
+  types: [blockContentType, examplePersonType, exampleDatasetType, peopleType],
+};
